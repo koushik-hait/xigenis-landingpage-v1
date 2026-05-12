467bb10e2d0c0e30ca5bb9e82a988365d609ed0f
koushik <koushikhait49@gmail.com>
2026-05-12 23:01:39 +0530

    toaster added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import "styles/tailwind.css"
+import { Toaster } from "@/components/ui/sonner"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <>
+            {children}
+          </>
+          <Toaster />
+        </body>
       </html>
     </ClerkProvider>
   )
