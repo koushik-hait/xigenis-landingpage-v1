63d27432b312fe9b62a5ad785f6137b5a4235034
koushik-hait <koushikhait49@gmail.com>
2026-07-07 20:08:42 +0530

    agentation added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import "styles/tailwind.css"
 import { Toaster } from "@/components/ui/sonner"
+import { Agentation } from "agentation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             {children}
           </>
           <Toaster />
+          <Agentation />
         </body>
       </html>
     </ClerkProvider>
